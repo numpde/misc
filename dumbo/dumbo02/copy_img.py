195bef6a07de37ba85e73ee49ada14b69f989c33
r <21158052+numpde@users.noreply.github.com>
2019-09-29 00:03:38 +0200

    RA: Dumbo 2

--- dumbo/dumbo02/copy_img.py
+++ dumbo/dumbo02/copy_img.py
@@ -11,6 +11,7 @@ PARAM = {
 	'url': (open("UV/url.txt", 'r').readlines()).pop(0),
 	'data': "UV/products.csv",
 
+	'color': "UV/src/{color}.JPG",
 	'path_src': "UV/src/{name}.JPG",
 	'path_trg': "UV/trg/{name}.JPG",
 	'path_zip': "UV/zip/{name}.zip",
@@ -23,6 +24,8 @@ with urllib.request.urlopen(PARAM['url']) as response:
 df = pd.read_csv(PARAM['data'])
 # print(df.head())
 
+df['Combine'] = df['Combine'].fillna(0)
+
 def filename_src(name):
 	return PARAM['path_src'].format(name=name)
 
@@ -32,43 +35,63 @@ def filename_trg(sku):
 def filename_zip(sku):
 	return PARAM['path_zip'].format(name=sku)
 
-for (i, row) in df.iterrows():
-
-	try:
-		src = filename_trg(row['Name'])
-		trg = filename_trg(row['Code'])
-
-		print("Copying {} to {}".format(src, trg))
-
-		shutil.copyfile(src, trg)
-	except:
-		print("FAILED")
-
-
-
-# # ZIP ALL
 # for (i, row) in df.iterrows():
-# 	print("----")
-#
-# 	if (row['Zip'] == 'nozip'):
-# 		print("Skipping (nozip): {}".format(row['Code']))
-# 		continue
-#
-# 	assert(np.isnan(row['Zip']))
-#
-# 	(a, b) = (filename_trg(row['Code']), filename_zip(row['Code']))
-#
-# 	if os.path.isfile(b):
-# 		print("Zip already exists, skipping: {}".format(b))
-# 		continue
-#
-# 	if not os.path.isfile(a):
-# 		print("File not found: {}".format(a))
-# 		continue
 #
 # 	try:
-# 		print("Zipping {} to {}".format(a, b))
-# 		zipfile.ZipFile(b, mode='w').write(a, os.path.basename(a))
-# 		print("OK")
+# 		src = filename_src(row['Name'])
+# 		trg = filename_trg(row['Code'])
+#
+# 		print("Copying {} to {}".format(src, trg))
+#
+# 		shutil.copyfile(src, trg)
 # 	except:
 # 		print("FAILED")
+
+
+for (k, group) in df.groupby(['Product']):
+	group: pd.DataFrame
+
+	try:
+		assert(1 == len(set(group['Combine'])))
+	except:
+		print("Error for group: {}".format(k))
+		print(group)
+		raise
+
+	if any(group['Combine']):
+
+		src = list(map(filename_src, set(group['Name'])))
+
+		try:
+			src1 = {os.path.isfile(s): s for s in src}[True]
+		except:
+			print("No source for {}".format(src))
+			continue
+
+		for (i, row) in group.iterrows():
+			src2 = PARAM['color'].format(color=row['Color'])
+			trg = filename_zip(row['Code'])
+			name1 = row['Product'] + ".JPG"
+			name2 = row['Name'] + ".JPG"
+			print("Combine {} and {} to {}".format(src1, src2, trg))
+
+			with zipfile.ZipFile(trg, mode='w') as fd:
+				fd.write(src1, name1)
+				fd.write(src2, name2)
+
+	else:
+
+		for (i, row) in group.iterrows():
+			src = filename_src(row['Name'])
+			zip = filename_zip(row['Code'])
+
+			print("Zipping {} to {}".format(src, zip))
+
+			if not os.path.isfile(src):
+				print("Not found: {}".format(src))
+				continue
+
+			with zipfile.ZipFile(zip, mode='w') as fd:
+				fd.write(src, os.path.basename(src))
+
+
